2b1353e1d1067c1f228f0af957e76215df4b0a0a
VIctor <colen.victor@icloud.com>
2021-12-17 18:40:02 -0300

    v 1.0 done

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct CheckoutView: View {
     @ObservedObject var order: Order
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
     
     var body: some View {
         ScrollView {
@@ -25,14 +27,42 @@ struct CheckoutView: View {
                 Text("Your total is \(order.cost, format: .currency(code: "USD"))")
                     .font(.title)
                 
-                Button("Placeholder") {
-                    
+                Button("Place Order") {
+                    Task {
+                        await placeOrder()
+                    }
                 }
                 .padding()
             }
         }
+        .alert("Thank you!", isPresented: $showingConfirmation) {
+            Button("Ok") { }
+        }  message: {
+            Text(confirmationMessage)
+        }
         .navigationTitle("Check out")
         .navigationBarTitleDisplayMode(.inline)
+    }
+    func placeOrder() async {
+        guard let encoder = try? JSONEncoder().encode(order) else {
+            print("Couldn't encode data")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoder)
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order for \(decodedOrder.quantity)X \(Order.types[decodedOrder.type].lowercased()) cupcakes is on it's way!"
+            showingConfirmation = true
+        } catch {
+            print("Checkout failed.")
+        }
     }
 }
 
